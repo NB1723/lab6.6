a1b7234d0e8167e550884cfa8eaa29cfff468303
NB1723 <notalia03@gmail.com>
2021-05-24 07:42:29 +0300

    lab6.6

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Queue q;
+			q.push(4);
+			q.push(3);
+			q.push(9);
+			Assert::AreEqual(q.pop(), 4);
+			Assert::AreEqual(q.pop(), 3);
+			Assert::AreEqual(q.pop(), 9);
+		}
+	};
+}
